1d927da50579f3d5b77d4a64a68b61586308f6da
Rolland Yann <rollandyann@gmail.com>
2018-05-28 14:00:45 +0200

    Up

--- app/js/main.js
+++ app/js/main.js
@@ -87,6 +87,14 @@ jQuery(document).ready(UTIL.loadEvents);
 ==========================================================================
 \*======================================================================*/
 /*
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+<script>
+  WebFont.load({
+    google: {
+      families: ['Fjalla One']
+    }
+  });
+  
 var cssId = 'global-on-demand';  // you could encode the css path itself to generate id..
 if (!document.getElementById(cssId)){
     var head  = document.getElementsByTagName('head')[0];
@@ -94,7 +102,19 @@ if (!document.getElementById(cssId)){
     link.id   = cssId;
     link.rel  = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'https://rawgit.com/fluxinetwork/nc-partners-hive/master/app/css/main.css';
+    link.href = 'https://cdn.rawgit.com/fluxinetwork/nc-partners-hive/2d687796/app/css/main.css';
     link.media = 'all';
     head.appendChild(link);
-}*/
+}
+
+$( document ).ready(function() {
+    $('.homepage-network__container .visuallyhidden--palm').removeClass('visuallyhidden--palm');
+    
+    $('.js-read-more').on('click', function(e){
+        e.preventDefault();
+        $(this).next().slideToggle();
+        $(this).hide();
+    });
+});
+
+</script>*/
