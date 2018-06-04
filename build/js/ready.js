49bef267f47958795dc5815eaabc19f34bb58789
Rolland Yann <rollandyann@gmail.com>
2018-06-04 12:11:51 +0200

    Up

--- build/js/ready.js
+++ build/js/ready.js
@@ -8,7 +8,22 @@
 
 $( document ).ready(function() {
     $('.homepage-network__container .visuallyhidden--palm').removeClass('visuallyhidden--palm');
+
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+            }
+        }
+    });
 });
+
+
 
 
 /*
